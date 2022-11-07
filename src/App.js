b90dab6b342283a5dee6c6a092a76db50e2d072f
gonzalo <gonzalogarciadev@gmail.com>
2022-11-06 23:20:19 -0500

    Initial setup for pokedex app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import PokemonList from './components/PokemonList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+        <div class="mainBody">
+            <div class="blueBall"></div>
+            <div class="red miniBall"></div>
+            <div class="yellow miniBall"></div>
+            <div class="green miniBall"></div>
+            <div class="mainScreen">
+                <div class="imageContainer">
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png" alt="pika"/>
+                </div>
+                <div class="dotLeft"></div>
+                <div class="dotRight"></div>
+                <div class="decliveBottom"></div>
+            </div>
+            <div class="searchTab">
+                <form action="">
+                    <input type="text"/>
+                    <input type="submit" />
+                </form>
+            </div>
+            <div class="crossY"></div>
+            <div class="crossX"></div>
+            <div class="greenScreen"></div>
+        </div>
+        <div class="secondaryBody">
+            <div class="overHead"></div>
+            <div class="declive"></div>
+            <div class="secondaryScreen">
+                <div class="base">
+                    <ul>
+                        <PokemonList />
+                        <PokemonList />
+                        <PokemonList />
+                        <PokemonList />
+                        <PokemonList />
+                    </ul>
+                    <div class="pokemonSelector">
+                        <img src="https://imagenpng.com/wp-content/uploads/2016/09/Pokebola-pokeball-png-2.png" alt="pokeball" />
+                    </div>
+                </div>
+            </div>
+            <div class="blueGroup">
+                <div class="squareGroup">
+                    <div class="square"></div>
+                    <div class="square"></div>
+                    <div class="square"></div>
+                    <div class="square"></div>
+                    <div class="square"></div>
+                </div>
+                <div class="squareGroup">
+                    <div class="square"></div>
+                    <div class="square"></div>
+                    <div class="square"></div>
+                    <div class="square"></div>
+                    <div class="square"></div>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
---
+++ src/components/PokemonList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PokemonList = () => {
+  return (
+    <li>002 POKEMON</li>
+  )
+}
+
+export default PokemonList
