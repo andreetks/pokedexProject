644c0a87a77f6e26d5bb612f051353aabceb0795
gonzalo <gonzalogarciadev@gmail.com>
2022-11-07 10:28:42 -0500

    Fix list styles in secondary screen

--- src/App.js
+++ src/App.js
@@ -1,66 +1,75 @@
-import './App.css';
-import PokemonList from './components/PokemonList';
+import "./App.css";
+import PokemonList from "./components/PokemonList";
 
-function App() {
+const App = () => {
   return (
     <div class="container">
-        <div class="mainBody">
-            <div class="blueBall"></div>
-            <div class="red miniBall"></div>
-            <div class="yellow miniBall"></div>
-            <div class="green miniBall"></div>
-            <div class="mainScreen">
-                <div class="imageContainer">
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png" alt="pika"/>
-                </div>
-                <div class="dotLeft"></div>
-                <div class="dotRight"></div>
-                <div class="decliveBottom"></div>
-            </div>
-            <div class="searchTab">
-                <form action="">
-                    <input type="text"/>
-                    <input type="submit" />
-                </form>
-            </div>
-            <div class="crossY"></div>
-            <div class="crossX"></div>
-            <div class="greenScreen"></div>
+      <div class="mainBody">
+        <div class="blueBall"></div>
+        <div class="red miniBall"></div>
+        <div class="yellow miniBall"></div>
+        <div class="green miniBall"></div>
+        <div class="mainScreen">
+          <div class="imageContainer">
+            <img
+              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
+              alt="pika"
+            />
+          </div>
+          <div class="dotLeft"></div>
+          <div class="dotRight"></div>
+          <div class="decliveBottom"></div>
         </div>
-        <div class="secondaryBody">
-            <div class="overHead"></div>
-            <div class="declive"></div>
-            <div class="secondaryScreen">
-                <div class="base">
-                    <ul>
-                        <PokemonList />
-                        <PokemonList />
-                        <PokemonList />
-                        <PokemonList />
-                        <PokemonList />
-                    </ul>
-                    <div class="pokemonSelector">
-                        <img src="https://imagenpng.com/wp-content/uploads/2016/09/Pokebola-pokeball-png-2.png" alt="pokeball" />
-                    </div>
-                </div>
-            </div>
-            <div class="blueGroup">
-                <div class="squareGroup">
-                    <div class="square"></div>
-                    <div class="square"></div>
-                    <div class="square"></div>
-                    <div class="square"></div>
-                    <div class="square"></div>
-                </div>
-                <div class="squareGroup">
-                    <div class="square"></div>
-                    <div class="square"></div>
-                    <div class="square"></div>
-                    <div class="square"></div>
-                    <div class="square"></div>
-                </div>
-            </div>
+        <div class="searchTab">
+          <form action="">
+            <input type="text" />
+            <input type="submit" />
+          </form>
         </div>
+        <div class="crossY"></div>
+        <div class="crossX"></div>
+        <div class="greenScreen"></div>
+      </div>
+      <div class="secondaryBody">
+        <div class="overHead"></div>
+        <div class="declive"></div>
+        <div class="secondaryScreen">
+          <div class="base">
+            <ul>
+              <PokemonList />
+              <PokemonList />
+              <PokemonList />
+              <PokemonList />
+              <PokemonList />
+              <PokemonList />
+              <PokemonList />
+              <PokemonList />
+            </ul>
+            <div class="pokemonSelector">
+              <img
+                src="https://imagenpng.com/wp-content/uploads/2016/09/Pokebola-pokeball-png-2.png"
+                alt="pokeball"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="blueGroup">
+          <div class="squareGroup">
+            <div class="square"></div>
+            <div class="square"></div>
+            <div class="square"></div>
+            <div class="square"></div>
+            <div class="square"></div>
+          </div>
+          <div class="squareGroup">
+            <div class="square"></div>
+            <div class="square"></div>
+            <div class="square"></div>
+            <div class="square"></div>
+            <div class="square"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const PokemonList = () => {
   return (
-    <li>002 POKEMON</li>
-  )
-}
+    <>
+      <li>
+        <button className="pokemonSelect" style={{
+          width: "100%",
+          height: "100%",
+          padding: "14px 0",
+          // border: 0
+        }}>
+          <img
+            style={{
+              display: "",
+              width: "40px",
+              position: "absolute",
+              left: "11px",
+              top: "3px",
+            }}
+            src="https://imagenpng.com/wp-content/uploads/2016/09/Pokebola-pokeball-png-2.png"
+            alt="pokeball"
+            className="unselected"
+          />
+          002 POKEMON
+        </button>
+      </li>
+    </>
+  );
+};
 
-export default PokemonList
+export default PokemonList;
